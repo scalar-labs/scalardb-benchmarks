511100e9fd773f16e00c2ac9d04bdac444d0f4d8
Hiroyuki Yamada <mogwaing@gmail.com>
2026-01-30 22:15:45 +0900

    Fix

--- src/main/java/com/scalar/db/benchmarks/ycsb/WorkloadFWithVirtual.java
+++ src/main/java/com/scalar/db/benchmarks/ycsb/WorkloadFWithVirtual.java
@@ -38,7 +38,7 @@ public class WorkloadFWithVirtual extends TimeBasedProcessor {
     this.manager = Common.getTransactionManager(config);
     this.recordCount = getRecordCount(config);
     this.opsPerTx = (int) config.getUserLong(CONFIG_NAME, OPS_PER_TX, DEFAULT_OPS_PER_TX);
-    this.payloadSize = getPayloadSize(config) / 2;
+    this.payloadSize = getPayloadSize(config);
   }
 
   @Override
